a31d08c310ae9c6526df58dfb9c38b3321d9a2bb
lorenzodedo <61524729+lorenzodedo@users.noreply.github.com>
2020-12-04 00:43:59 +0100

    Add files via upload

--- switch.js
+++ switch.js
@@ -1,6 +1,51 @@
-$('[lang="small"]').hide();
+//LOADER
+$(window).on("load",function(){
+  $(".loader-wrapper").fadeOut("slow");
+});
 
-if ($(window).width() < 768) {
-  $('[lang="big"]').hide();
-  $('[lang="small"]').show();
+// searchbox
+function myFunction() {
+  // Declare variables
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById('myInput');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myUL");
+  li = ul.getElementsByTagName('li');
+
+  // Hide countries when no input is given
+  if (input.value.length == 0) {
+    ul.style.display = "none";
+    return;
+  } else {
+    ul.style.display = "block";
+  }
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "block";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
 }
+
+
+// FAQs
+const items = document.querySelectorAll(".accordion button");
+
+function toggleAccordion() {
+  const itemToggle = this.getAttribute('aria-expanded');
+
+  for (i = 0; i < items.length; i++) {
+    items[i].setAttribute('aria-expanded', 'false');
+  }
+
+  if (itemToggle == 'false') {
+    this.setAttribute('aria-expanded', 'true');
+  }
+}
+
+items.forEach(item => item.addEventListener('click', toggleAccordion));
